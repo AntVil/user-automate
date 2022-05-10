d6a450b065cfec18d07dea762ee1920877be3696
Anton Böhler <antboehler@gmail.com>
2022-05-10 20:13:12 +0200

    update

--- src/interface/script.js
+++ src/interface/script.js
@@ -1,67 +1,138 @@
 window.onload = function(){
     
 
-    setupActionsList();
+    setupAutomationsList();
 }
 
-function createAction(id, name){
-    let action = document.createElement("div");
-    let actionRun = document.createElement("button");
-    let actionName = document.createElement("input");
-    let actionEdit = document.createElement("button");
-    let actionDelete = document.createElement("button");
+function createAutomation(id, name){
+    let automation = document.createElement("div");
+    let automationRun = document.createElement("button");
+    let automationName = document.createElement("input");
+    let automationEdit = document.createElement("button");
+    let automationDelete = document.createElement("button");
 
-    action.id = `${id}`;
-    actionRun.innerText = "▶";
-    actionName.value = name;
-    actionEdit.innerText = "⚙";
-    actionDelete.innerText = "x";
+    automation.id = `${id}`;
+    automationRun.innerText = "▶";
+    automationName.value = name;
+    automationEdit.innerText = "⚙";
+    automationDelete.innerText = "x";
 
-    actionRun.onclick = async () => {await eel.action_execute(parseInt(action.id))};
-    actionName.onchange = async () => {await eel.action_rename(parseInt(action.id), actionName.value)};
-    actionEdit.onclick = () => console.log("edit");
-    actionDelete.onclick = async () => {
-        let siblingAfter = action.nextElementSibling;
-        while(siblingAfter.id !== "new-action"){
+    automationRun.onclick = async () => {await eel.automation_execute(parseInt(automation.id))};
+    automationName.onchange = async () => {await eel.automation_rename(parseInt(automation.id), automationName.value)};
+    automationEdit.onclick = async () => {await editAutomation(parseInt(automation.id))};
+    automationDelete.onclick = async () => {
+        let anwser = confirm(`do you really want to delete "${automationName.value}"?`);
+        if(!anwser){
+            return;
+        }
+
+        let siblingAfter = automation.nextElementSibling;
+        while(siblingAfter.id !== "new-automation"){
             siblingAfter.id = `${parseInt(siblingAfter.id) - 1}`;
             siblingAfter = siblingAfter.nextElementSibling;
         }
 
-        action.remove();
-        let index = parseInt(action.id);
-        await eel.action_delete(index);
+        automation.remove();
+        let index = parseInt(automation.id);
+        await eel.automation_delete(index);
     };
 
-    action.appendChild(actionRun);
-    action.appendChild(actionName);
-    action.appendChild(actionEdit);
-    action.appendChild(actionDelete);
+    automation.appendChild(automationRun);
+    automation.appendChild(automationName);
+    automation.appendChild(automationEdit);
+    automation.appendChild(automationDelete);
 
-    return action;
+    return automation;
 }
 
-async function setupActionsList(){
-    let actions = await eel.get_actions_list()();
+async function setupAutomationsList(){
+    let automations = await eel.automations_list_get()();
 
-    let actionsList = document.getElementById("actions-list");
-    let newAction = document.getElementById("new-action");
-    for(let action of actionsList.children){
-        if(action !== newAction){
-            actionsList.removeChild(action);
+    let automationsList = document.getElementById("automations-list");
+    let newAutomation = document.getElementById("new-automation");
+    for(let automation of automationsList.children){
+        if(automation !== newAutomation){
+            automationsList.removeChild(automation);
         }
     }
 
-    for(let i=0;i<actions.length;i++){
-        actionsList.insertBefore(createAction(i, actions[i]), newAction);
+    for(let i=0;i<automations.length;i++){
+        automationsList.insertBefore(createAutomation(i, automations[i]), newAutomation);
     }
 }
 
 
-async function addAction(){
+async function addAutomation(){
+    let automationsList = document.getElementById("automations-list");
+    let newAutomation = document.getElementById("new-automation");
+    let index = automationsList.children.length - 1;
+    let name = "new automation";
+    automationsList.insertBefore(createAutomation(index, name), newAutomation);
+    await eel.automation_create(index, name);
+}
+
+
+async function editAutomation(index){
+    let automation = await eel.automation_get(index)();
+    
+    document.getElementById("automation-info-id").value = index;
+    document.getElementById("automation-info-name").value = automation.name;
+    for(let flag of automation.flags){
+        try{
+            let [id, value] = flag.split(":");
+            document.getElementById(`automation-info-${id}`).value = value.trim();
+        }catch{
+            console.log("some feature might have not been implemented.");
+        }
+    }
+
+
+    document.getElementById("start-record").disabled = false;
+    document.getElementById("automation-info-cancel").disabled = false;
+    document.getElementById("automation-info-save").disabled = false;
+    document.getElementById("stop-record").disabled = true;
+
+    document.getElementById("editing-automation").checked = true;
+};
+
+
+async function saveAutomation(){
+    let index = parseInt(document.getElementById("automation-info-id").value);
+    await eel.save_automation(index);
+    
+    console.log("saved");
+
+    document.getElementById("editing-automation").checked = false;
+}
+
+function cancelAutomation(){
+    document.getElementById("editing-automation").checked = false;
+}
+
+
+async function startRecord(){
+    document.getElementById("start-record").disabled = true;
+    document.getElementById("automation-info-cancel").disabled = true;
+    document.getElementById("automation-info-save").disabled = true;
+    document.getElementById("stop-record").disabled = false;
+
+    await eel.record_start();
+}
+
+
+async function stopRecord(){
+    let automationActions = await eel.record_stop()();
+
     let actionsList = document.getElementById("actions-list");
-    let newAction = document.getElementById("new-action");
-    let index = actionsList.children.length - 1;
-    let name = "new action";
-    actionsList.insertBefore(createAction(index, name), newAction);
-    await eel.action_create(index, name);
+    for(let actionContent of automationActions){
+        let action = document.createElement("div");
+        action.innerText = actionContent;
+        action.setAttribute("data-content", actionContent[0])
+        actionsList.appendChild(action);
+    }
+    
+    document.getElementById("start-record").disabled = true;
+    document.getElementById("automation-info-cancel").disabled = false;
+    document.getElementById("automation-info-save").disabled = false;
+    document.getElementById("stop-record").disabled = true;
 }
--- src/main.py
+++ src/main.py
@@ -1,52 +1,148 @@
-import keyboard
-import mouse
+from pynput import keyboard
+from pynput import mouse
 import eel
 import os
+import datetime
 
 
-ACTIONS_FOLDER = os.path.join(os.path.dirname(__file__), "actions")
+AUTOMATIONS_FOLDER = os.path.join(os.path.dirname(__file__), "automations")
 INTERFACE_FOLDER = os.path.join(os.path.dirname(__file__), "interface")
 
 
 
 
 
+#mouse.on_click(record_click)
+
 @eel.expose
-def get_actions_list():
-    actions = []
-    for file in os.listdir(ACTIONS_FOLDER):
-        with open(os.path.join(ACTIONS_FOLDER, file), "r") as f:
+def automations_list_get():
+    automations = []
+    for file in os.listdir(AUTOMATIONS_FOLDER):
+        with open(os.path.join(AUTOMATIONS_FOLDER, file), "r") as f:
             name = f.readline()
-        actions.append(name)
-    return actions
+        automations.append(name)
+    return automations
 
 
 @eel.expose
-def action_execute(index):
+def automation_execute(index):
     pass
 
 
 @eel.expose
-def action_rename(index, name):
-    with open(os.path.join(ACTIONS_FOLDER, f"{index}.txt"), "r") as f:
+def automation_rename(index, name):
+    with open(os.path.join(AUTOMATIONS_FOLDER, f"{index}.txt"), "r") as f:
         lines = f.readlines()
     lines[0] = f"{name}\n"
-    with open(os.path.join(ACTIONS_FOLDER, f"{index}.txt"), "w") as f:
+    with open(os.path.join(AUTOMATIONS_FOLDER, f"{index}.txt"), "w") as f:
         f.writelines(lines)
 
 
 @eel.expose
-def action_delete(index):
-    os.remove(os.path.join(ACTIONS_FOLDER, f"{index}.txt"))
-    for file in sorted(os.listdir(ACTIONS_FOLDER))[index:]:
+def automation_delete(index):
+    os.remove(os.path.join(AUTOMATIONS_FOLDER, f"{index}.txt"))
+    for file in sorted(os.listdir(AUTOMATIONS_FOLDER))[index:]:
         i = int(file.split(".")[0]) - 1
-        os.rename(os.path.join(ACTIONS_FOLDER, file), os.path.join(ACTIONS_FOLDER, f"{i}.txt"))
+        os.rename(os.path.join(AUTOMATIONS_FOLDER, file), os.path.join(AUTOMATIONS_FOLDER, f"{i}.txt"))
 
 
 @eel.expose
-def action_create(index, name):
-    with open(os.path.join(ACTIONS_FOLDER, f"{index}.txt"), "w") as f:
+def automation_create(index, name):
+    with open(os.path.join(AUTOMATIONS_FOLDER, f"{index}.txt"), "w") as f:
         f.write(f"{name}\n")
+
+
+@eel.expose
+def automation_get(index):
+    with open(os.path.join(AUTOMATIONS_FOLDER, f"{index}.txt"), "r") as f:
+        lines = f.readlines()
+    
+    name = lines[0].strip()
+    flags = []
+    actions = []
+    for line in lines[1:]:
+        if line.startswith("-"):
+            flags.append(line[1:].strip())
+        else:
+            actions.append(line.strip())
+    
+    return {
+        "name": name,
+        "flags": flags,
+        "actions": actions
+    }
+
+
+@eel.expose
+def automation_save(index):
+    pass
+
+
+
+
+recording = []
+recording_start = datetime.datetime.now()
+
+def record_mousemove(x, y):
+    if len(recording) > 0 and recording[-1][0] == "move":
+        recording[-1][1].append([x, y, datetime.datetime.now()])
+    else:
+        recording.append(("move", [[x, y, datetime.datetime.now()]]))
+
+
+def record_click(x, y, button, is_press):
+    if is_press:
+        recording.append(["click", button.name, datetime.datetime.now()])
+
+
+def record_keypress(char):
+    if len(recording) > 0 and recording[-1][0] == "press" and recording[-1][1] == char:
+        return
+    recording.append(["press", str(char).lower(), datetime.datetime.now()])
+
+
+def record_keyrelease(char):
+    if len(recording) > 0 and recording[-1][0] == "release" and recording[-1][1] == char:
+        return
+    recording.append(["release", str(char).lower(), datetime.datetime.now()])
+
+
+@eel.expose
+def record_start():
+    global mouse_listener
+    global keyboard_listener
+    global recording
+    global recording_start
+    recording = []
+    mouse_listener = mouse.Listener(record_mousemove, record_click)
+    keyboard_listener = keyboard.Listener(record_keypress, record_keyrelease)
+    recording_start = datetime.datetime.now()
+    mouse_listener.start()
+    keyboard_listener.start()
+    
+
+@eel.expose
+def record_stop():
+    global mouse_listener
+    global keyboard_listener
+    global recording
+    global recording_start
+    mouse_listener.stop()
+    keyboard_listener.stop()
+
+    dt = recording_start
+    for action in recording:
+        if action[0] == "move":
+            for move in action[1]:
+                dt_ = move[2]
+                move[2] = (move[2] - dt).total_seconds()
+                dt = dt_
+        else:
+            dt_ = action[2]
+            action[2] = (action[2] - dt).total_seconds()
+            dt = dt_
+    return recording
+        
 
 
 eel.init(INTERFACE_FOLDER)
